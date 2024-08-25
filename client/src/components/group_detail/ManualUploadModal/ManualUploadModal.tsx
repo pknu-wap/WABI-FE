766cef8dcb019d244a41fdc6d335d441a4ba6666
Junseo Kim <semiwest2@gmail.com>
2024-08-25 18:57:05 +0900

    #6 ManualUploadModal.tsx
    인원 수동 추가 모달 생성
    
    Co-Authored-By: Jang Hong Joon <35947667+wkdghdwns199@users.noreply.github.com>

---
+++ client/src/components/group_detail/ManualUploadModal/ManualUploadModal.tsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import Steps from './steps/Steps';
+import {student} from "../../../types/studentTypes";
+
+
+const ManualUploadModal = () => {
+    const [step, setStep] = useState(1);
+    const nextStep = () => setStep(step + 1);
+    const prevStep = () => setStep(step - 1);
+    const [student, setStudent] = useState<student>(
+        {
+            studentId: '',
+            name: '',
+            club: '',
+            position: '',
+            joinDate: '',
+            college: '',
+            major: '',
+            tel: '',
+            status: '',
+        });
+
+    const handleChange = (studentInfoInput: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = studentInfoInput.target;
+        setStudent((prevStudent) => ({
+            ...prevStudent,
+            [name]: value,
+        }));
+        console.log(student);
+    };
+
+    return (
+        <div>
+            <Steps step={step} nextStep={nextStep} prevStep={prevStep} student={student} handleChange={handleChange}/>
+
+        </div>
+    )
+}
+
+//
+// <input
+//     type="text"
+//     name="username"
+//     value={사용자가 }
+//     onChange={handleChange}
+// />
+
+
+export default ManualUploadModal;
