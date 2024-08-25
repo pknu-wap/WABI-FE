fc88128b90b48232ac7418ca01867a7670451bf1
Junseo Kim <semiwest2@gmail.com>
2024-08-25 18:59:39 +0900

    #6 studentType.ts 생성
    학생 정보들의 타입을 정의
    
    Co-Authored-By: Jang Hong Joon <35947667+wkdghdwns199@users.noreply.github.com>

---
+++ client/src/types/studentTypes.ts
@@ -0,0 +1,13 @@
+export type student = {
+    studentId: string,
+    name: string,
+    club: string,
+    position: string,
+    joinDate: string,
+    college: string,
+    major: string,
+    tel: string,
+    status: string,
+}
+
+export type students = student[];
