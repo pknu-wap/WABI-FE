dcd2a8c8feb78a4335c790452b99e42ff8c19d7a
wkdghdwns199 <wkdghdwns199@naver.com>
2024-08-25 18:45:12 +0900

    #4 postman mock 서버 주소로 파일 전송 테스트
    fix : localhost 서버에서 postman mock 서버 주소로 변경하여 파일 전송 테스트
    Co-authored-by: oesnuj <semiwest2@gmail.com>

--- client/src/api/fileUpload.ts
+++ client/src/api/fileUpload.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fileUpload = (fileFormData : FormData) => {
     console.log(Array.from(fileFormData))
-    axios.post('http://localhost:8000/upload-endpoint',fileFormData, {
+    axios.post('https://13f9350d-c685-4634-9131-e118c99027d6.mock.pstmn.io/api/bands/1/members/enrollments/file',fileFormData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
