12705fcc18d1f862a0a7f4c579bcdd5b6eb3960d
wkdghdwns199 <wkdghdwns199@naver.com>
2024-08-28 15:54:06 +0900

    #10 docs : TODO 주석 작성
    추후 해야 할 일 TODO로 작성
    
    Co-authored-by: 김준서 Junseo Kim <semiwest2@gmail.com>

--- client/src/pages/GroupDetail/GroupDetail.tsx
+++ client/src/pages/GroupDetail/GroupDetail.tsx
@@ -9,11 +9,10 @@ import Header from 'components/common/Header/Header';
 //Todo
 //1. ModalFrame 만들기
 //2. ModalFrame 활용해서 수동추가, 자동추가 모달 완성하기
-//----------------이제부터 group_detail.tsx부분
-//3. Group Name
+//3. Group Name 부분 만들기
 //4. 검색 박스 컴포넌트 + 인원 자동/수동 추가 부분 하나의 컴포넌트로 만들기
 //5. GroupMemberList 부분 컴포넌트 만들기
-//6. 1~3 하나로 묶는다.
+//6. 3~5 하나로 묶기.
 
 
 const GroupDetailPage = () => {
