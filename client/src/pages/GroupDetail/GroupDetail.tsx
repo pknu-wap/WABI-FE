2793b7605deb5e091d76814dd26152dcf962141c
wkdghdwns199 <wkdghdwns199@naver.com>
2024-08-28 15:49:17 +0900

    #10 fix : GroupDetail 페이지 컴포넌트 배치
    GroupDetail 페이지에 필요한 컴포넌트들 생성 및 배치
    
    Co-authored-by: 김준서 Junseo Kim <semiwest2@gmail.com>

--- client/src/pages/GroupDetail/GroupDetail.tsx
+++ client/src/pages/GroupDetail/GroupDetail.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import FileUploadModal from '../../components/group_detail/FileUploadModal/FileUploadModal';
-
+import MemberUpdateButton from 'components/group_detail/MemberUpdateButton/MemberUpdateButton';
+import Header from 'components/common/Header/Header';
 // 파일 업로드 기능 컴포넌트
 const GroupDetailPage = () => {
     return (
+        <div>
+            <Header />
+            {/*<Title />*/}
 
-        <FileUploadModal/>
+            <div>
+                {/*<SerchBox/>*/}
+                <div>
+                    <MemberUpdateButton />
+                    {/*<ManualUpdateButton />*/}
+                </div>
+                {/*<GroupMemberList />*/}
+            </div>
+            {/*<FileUploadModal/>*/}
+        </div>
     );
 }
 
