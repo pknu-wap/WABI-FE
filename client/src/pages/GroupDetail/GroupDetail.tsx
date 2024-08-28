c52cce5a2cc3aaac03f66b4426c7f1626dd475d6
wkdghdwns199 <wkdghdwns199@naver.com>
2024-08-28 15:49:54 +0900

    #10 docs : TODO 주석 작성
    추후 해야 할 일 TODO로 작성
    
    Co-authored-by: 김준서 Junseo Kim <semiwest2@gmail.com>

--- client/src/pages/GroupDetail/GroupDetail.tsx
+++ client/src/pages/GroupDetail/GroupDetail.tsx
@@ -3,6 +3,19 @@ import FileUploadModal from '../../components/group_detail/FileUploadModal/FileU
 import MemberUpdateButton from 'components/group_detail/MemberUpdateButton/MemberUpdateButton';
 import Header from 'components/common/Header/Header';
 // 파일 업로드 기능 컴포넌트
+
+
+
+//Todo
+//1. ModalFrame 만들기
+//2. ModalFrame 활용해서 수동추가, 자동추가 모달 완성하기
+//----------------이제부터 group_detail.tsx부분
+//3. Group Name
+//4. 검색 박스 컴포넌트 + 인원 자동/수동 추가 부분 하나의 컴포넌트로 만들기
+//5. GroupMemberList 부분 컴포넌트 만들기
+//6. 1~3 하나로 묶는다.
+
+
 const GroupDetailPage = () => {
     return (
         <div>
