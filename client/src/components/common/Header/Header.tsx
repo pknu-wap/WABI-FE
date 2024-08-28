fb0b833a2b900de410d3a2f074d14c58bb987bfd
wkdghdwns199 <wkdghdwns199@naver.com>
2024-08-28 15:40:55 +0900

    #10 feature : Header 컴포넌트 생성
    서비스의 최상단에 배치될 컴포넌트들을 묶은 Header 컴포넌트 생성
    
    Co-authored-by: 김준서 Junseo Kim <semiwest2@gmail.com>

---
+++ client/src/components/common/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Logo from 'components/common/Logo/Logo';
+import {HeaderStyles, LogoGroup} from "components/common/Header/Header.styles";
+import icon from 'icon.png';
+import Button from 'components/common/Button/Button';
+const Header = () => {
+
+    return (
+        <HeaderStyles>
+            <LogoGroup>
+                <Logo src={icon} alt ={"logo1"} text={"WABI"} />
+                <Logo src={icon} alt ={"logo2"} text={"WAP"} />
+            </LogoGroup>
+            <Button
+                width="50px"
+                height="30px"
+                buttonColor="white"
+                fontColor="indigo"
+                fontSize="16px"
+            >
+                Login
+            </Button>
+        </HeaderStyles>
+    )
+}
+
+
+export default Header;
