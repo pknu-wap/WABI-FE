eb2745a1e8a84a878dd4ed97df43698e59fb512c
Junseo Kim <semiwest2@gmail.com>
2024-09-01 16:48:57 +0900

    #10 [feature] Logo components props로 classname 추가
    모달에서 사용하는 Logo 컴포넌트에서 css를 적용하기 위해 Logo 컴포넌트 props에 classname 추가
    
    Co-Authored-By: Jang Hong Joon <35947667+wkdghdwns199@users.noreply.github.com>

--- client/src/components/common/Logo/Logo.tsx
+++ client/src/components/common/Logo/Logo.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import {LogoContainer, LogoImage, LogoText} from 'components/common/Logo/Logo.styles';
+import {
+  LogoContainer,
+  LogoImage,
+  LogoText,
+} from 'components/common/Logo/Logo.styles';
 
 interface LogoProps {
-    src: string;
-    alt : string;
-    text: string;
+  className?: string;
+  src: string;
+  alt: string;
+  text: string;
 }
 
-const Logo = ({src, alt, text} : LogoProps) => {
-
-    return (
-        <div>
-            <LogoContainer>
-                <LogoImage src={src} alt={alt} />
-                <LogoText>{text}</LogoText>
-            </LogoContainer>
-
-        </div>
-    )
-}
-
+const Logo = ({className, src, alt, text}: LogoProps) => {
+  return (
+    <div className={className}>
+      <LogoContainer>
+        <LogoImage src={src} alt={alt} />
+        <LogoText>{text}</LogoText>
+      </LogoContainer>
+    </div>
+  );
+};
 
 export default Logo;
