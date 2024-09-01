4af4fb961cd4fad6010d14d65cc84a01799da53d
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-01 15:46:07 +0900

    App.tsx 원래상태로 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import QrScan from "./pages/main_qr_check_in/QrScan";
-import MainQrCheckIn from "./pages/main_qr_check_in/MainQrCheckIn";
 
 function App() {
     return (
         <div className="App">
-            <MainQrCheckIn/>
         </div>
     );
 }
