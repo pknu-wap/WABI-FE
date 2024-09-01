59efea20a1ad5e0c1872ce2ddd802f25b92c26c4
Junseo Kim <semiwest2@gmail.com>
2024-09-01 16:59:25 +0900

    #10 [fix] prettier 적용으로 인한 코드 정리
    Co-Authored-By: Jang Hong Joon <35947667+wkdghdwns199@users.noreply.github.com>

--- client/src/components/common/Header/Header.styles.ts
+++ client/src/components/common/Header/Header.styles.ts
@@ -1,19 +1,16 @@
 import styled from 'styled-components';
 export const HeaderStyles = styled.header`
-    width: 100%;
-    height: 55px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 20px;
-    box-sizing: border-box;
-    
-`
+  width: 100%;
+  height: 55px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+`;
 
-
-
-export const LogoGroup = styled.div`   
-    display: flex;
-    justify-content: space-between;
-    align-items: center
-`
+export const LogoGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
