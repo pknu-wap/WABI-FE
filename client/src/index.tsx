b7bc28e8a02aa411bca44f363ab23dea46c6cfbe
Junseo Kim <semiwest2@gmail.com>
2024-09-01 16:50:00 +0900

    #10 [feature] recoil 루트 추가
    Co-Authored-By: Jang Hong Joon <35947667+wkdghdwns199@users.noreply.github.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RecoilRoot} from 'recoil';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
+  <RecoilRoot>
     <App />
+  </RecoilRoot>,
 );
 
 // If you want to start measuring performance in your app, pass a function
