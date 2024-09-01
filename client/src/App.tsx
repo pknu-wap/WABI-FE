5b4bf494a1cb453d504ff8677e69e565bb51a570
Junseo Kim <semiwest2@gmail.com>
2024-09-01 17:06:37 +0900

    #10 [fix] 불필요 코드 제거
    Co-Authored-By: Jang Hong Joon <35947667+wkdghdwns199@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import GroupDetail from "./pages/GroupDetail/GroupDetail";
+import GroupDetail from './pages/GroupDetail/GroupDetail';
 
 function App() {
   return (
