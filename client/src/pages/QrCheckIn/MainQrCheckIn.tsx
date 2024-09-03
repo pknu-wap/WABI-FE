792f7595440f7c911c531a332bd8a4184397fa50
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-03 14:07:16 +0900

    스타일 컴포넌트 S로 묶어주기

--- client/src/pages/QrCheckIn/MainQrCheckIn.tsx
+++ client/src/pages/QrCheckIn/MainQrCheckIn.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import QrScan from './QrScan'
-import {Wrapper, Event, Date, QrStyle} from './QrCheckIn.styles';
+import * as S from './QrCheckIn.styles';
 
 const MainQrCheckIn = () => {
     return (
-       <Wrapper>
-           <Event>
+       <S.Wrapper>
+           <S.Event>
                <h2>EVENT NAME</h2>
                <div>
                    <h3>2024.08.05</h3>
                    <h3>2024.08.05</h3>
                 </div>
-                <Date>
+                <S.Date>
                     SELECT GROUP
-                </Date>
-           </Event>
+                </S.Date>
+           </S.Event>
            <div className="Qr">
-               <QrStyle><QrScan/></QrStyle>
+               <S.QrStyle><QrScan/></S.QrStyle>
                <h2>QR CODE를 화면의 사각형 안에 맞춰주세요.</h2>
            </div>
-       </Wrapper>
+       </S.Wrapper>
     )
 }
 
