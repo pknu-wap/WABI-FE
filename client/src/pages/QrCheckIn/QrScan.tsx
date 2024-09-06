fae276f3678ec085ef782d8fef2d07fd995fa299
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-06 10:54:17 +0900

    substring 내 숫자에서 이름으로 변경

--- client/src/pages/QrCheckIn/QrScan.tsx
+++ client/src/pages/QrCheckIn/QrScan.tsx
@@ -1,50 +1,49 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import QrScanner from 'react-qr-scanner';
-import {SendToServer} from "../../api/SendToServer";
-import {studentQr} from "../../types/QrType/StudentQr";
-import {scanData} from "../../types/QrType/ScanData";
-import {LoadCheckInStudent} from "../../api/LoadCheckInStudent";
+import {SendToServer} from '../../api/SendToServer';
+import {studentQr} from '../../types/QrType/StudentQr';
+import {scanData} from '../../types/QrType/ScanData';
+import {LoadCheckInStudent} from '../../api/LoadCheckInStudent';
 
 const Student: studentQr = {
-    studentId: "",
-    eventId: 1,
+  studentId: '',
+  eventId: 1,
 };
 
 const handleError = (err: Error) => {
-    console.error(err);
+  console.error(err);
 };
 
 const QrScan = () => {
+  const [scanned, setQrScanned] = useState(false);
 
-    const [scanned, setQrScanned] = useState(false);
+  const handleScan = (data: scanData) => {
+    if (data && !scanned) {
+      const ScannedQrArray = data.text;
+      const studentIdStartPoint = 8;
+      const studentIdEndPoint = 17;
+      const ExtractedStudentId = ScannedQrArray.substring(
+        studentIdStartPoint,
+        studentIdEndPoint,
+      );
+      Student.studentId = ExtractedStudentId;
+      SendToServer(Student);
 
-    const handleScan = (data: scanData) => {
-        if (data && !scanned) {
-            const ScannedQrArray = data.text;
-            const ExtractedStudentId = ScannedQrArray.substring(8,17);
-            Student.studentId = ExtractedStudentId;
-            SendToServer(Student);
+      LoadCheckInStudent();
 
-            LoadCheckInStudent()
+      setQrScanned(true);
 
-            setQrScanned(true);
+      setTimeout(() => {
+        setQrScanned(false);
+      }, 1000);
+    }
+  };
 
-            setTimeout(() => {
-                setQrScanned(false);
-            }, 1000);
-        }
-    };
-
-    return (
-        <div>
-            <QrScanner
-                onError={handleError}
-                onScan={handleScan}
-            />
-        </div>
-
-
-    );
-}
+  return (
+    <div>
+      <QrScanner onError={handleError} onScan={handleScan} delay={300} />
+    </div>
+  );
+};
 
 export default QrScan;
