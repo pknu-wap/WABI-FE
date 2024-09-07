f994a0532b5438f9040ee6ec815f0bc714d3d99b
wkdghdwns199 <wkdghdwns199@naver.com>
2024-09-07 20:28:17 +0900

    #13 docs : TODO 작성
    group_detail 페이지 UI 완성을 위한 TODO 작성
    
    Co-authored-by: 김준서 Junseo Kim <semiwest2@gmail.com>
    Co-authored-by: 서성원 <seongwon0903@gmail.com>

--- client/src/pages/GroupDetail/GroupDetail.tsx
+++ client/src/pages/GroupDetail/GroupDetail.tsx
@@ -14,12 +14,9 @@ import GroupMemberList from "../../components/common/GroupMemberList/GroupMember
 // 파일 업로드 기능 컴포넌트
 
 //Todo
-//1. ModalFrame 만들기
-//2. ModalFrame 활용해서 수동추가, 자동추가 모달 완성하기
-//3. Group Name 부분 만들기
-//4. 검색 박스 컴포넌트 + 인원 자동/수동 추가 부분 하나의 컴포넌트로 만들기
-//5. GroupMemberList 부분 컴포넌트 만들기
-//6. 3~5 하나로 묶기.
+//1. title, serchbox 컴포넌트 제작
+//2. title, serchbox랑 button, GroupMemberList 맞춰 정렬하기
+//3. group detail 페이지 css 마무리하기
 
 const GroupDetailPage = () => {
   const [fileUploadModalStateValue, setFileUploadModalStateValue] =
