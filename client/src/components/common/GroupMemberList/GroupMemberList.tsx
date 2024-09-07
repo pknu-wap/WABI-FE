114e0790d64b01065a843140ebd1358be86f284e
wkdghdwns199 <wkdghdwns199@naver.com>
2024-09-07 19:22:47 +0900

    #13 feature : GroupMemberList 컴포넌트 생성
    그룹에 속한 사람들 리스트를 보여주는 GroupMemberList 컴포넌트 생성
    
    Co-authored-by: 김준서 Junseo Kim <semiwest2@gmail.com>
    Co-authored-by: 서성원 <seongwon0903@gmail.com>

---
+++ client/src/components/common/GroupMemberList/GroupMemberList.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from "react";
+import * as Styled from "components/common/GroupMemberList/GroupMemberList.styles";
+import {loadGroupMemberList} from "../../../api/loadGroupMemberList";
+import GroupMembers from "./GroupMembers";
+
+// 학번, 이름 재학유무, 휴대폰 번호, 학과.전공, 체크
+const GroupMemberList = () => {
+    const [groupMembers, setGroupMembers] = useState([]);
+    useEffect(() => {
+        loadGroupMemberList(setGroupMembers)
+    },[])
+
+    useEffect(() => {
+
+    },[groupMembers])
+
+    return (
+        <div>
+            <Styled.Table>
+                <thead>
+                <tr>
+                    <Styled.ThData>학번</Styled.ThData>
+                    <Styled.ThData>이름</Styled.ThData> {/*여기에 실선 하나*/}
+                    <Styled.ThBorder>재학 유무</Styled.ThBorder>
+                    <Styled.ThData>휴대폰 번호</Styled.ThData>
+                    <Styled.ThData>학과.전공</Styled.ThData>
+                    <Styled.ThData>동아리</Styled.ThData>
+                    <Styled.ThData>직책</Styled.ThData>
+                    <Styled.ThData>가입일자</Styled.ThData>
+                    <Styled.ThData>소속대학</Styled.ThData>
+                    <Styled.ThBorder>체크</Styled.ThBorder>
+                </tr>
+                </thead>
+                <GroupMembers members={groupMembers} />
+            </Styled.Table>
+        </div>
+    )
+}
+
+export default GroupMemberList;
