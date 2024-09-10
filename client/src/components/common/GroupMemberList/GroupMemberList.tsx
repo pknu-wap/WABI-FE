7eac9abd1288126f3465fb907ba3ff6f8c8f39b4
wkdghdwns199 <wkdghdwns199@naver.com>
2024-09-10 22:18:17 +0900

    #13 refactor : Prettier 로컬 적용
    Prettier 로컬 적용 완료

--- client/src/components/common/GroupMemberList/GroupMemberList.tsx
+++ client/src/components/common/GroupMemberList/GroupMemberList.tsx
@@ -1,36 +1,36 @@
-import React, {useEffect, useState} from "react";
-import * as Styled from "components/common/GroupMemberList/GroupMemberList.styles";
-import {loadGroupMemberList} from "../../../api/loadGroupMemberList";
-import GroupMembers from "./GroupMembers";
+import React, {useEffect, useState} from 'react';
+import * as Styled from 'components/common/GroupMemberList/GroupMemberList.styles';
+import {loadGroupMemberList} from '../../../api/loadGroupMemberList';
+import GroupMembers from './GroupMembers';
 
 // 학번, 이름 재학유무, 휴대폰 번호, 학과.전공, 체크
 const GroupMemberList = () => {
-    const [groupMembers, setGroupMembers] = useState([]);
-    useEffect(() => {
-        loadGroupMemberList(setGroupMembers)
-    },[])
+  const [groupMembers, setGroupMembers] = useState([]);
+  useEffect(() => {
+    loadGroupMemberList(setGroupMembers);
+  }, []);
 
-    return (
-        <div>
-            <Styled.Table>
-                <thead>
-                <tr>
-                    <Styled.ThData>학번</Styled.ThData>
-                    <Styled.ThData>이름</Styled.ThData> {/*여기에 실선 하나*/}
-                    <Styled.ThBorder>재학 유무</Styled.ThBorder>
-                    <Styled.ThData>휴대폰 번호</Styled.ThData>
-                    <Styled.ThData>학과.전공</Styled.ThData>
-                    <Styled.ThData>동아리</Styled.ThData>
-                    <Styled.ThData>직책</Styled.ThData>
-                    <Styled.ThData>가입일자</Styled.ThData>
-                    <Styled.ThData>소속대학</Styled.ThData>
-                    <Styled.ThBorder>체크</Styled.ThBorder>
-                </tr>
-                </thead>
-                <GroupMembers members={groupMembers} />
-            </Styled.Table>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Styled.Table>
+        <thead>
+          <tr>
+            <Styled.ThData>학번</Styled.ThData>
+            <Styled.ThData>이름</Styled.ThData> {/*여기에 실선 하나*/}
+            <Styled.ThBorder>재학 유무</Styled.ThBorder>
+            <Styled.ThData>휴대폰 번호</Styled.ThData>
+            <Styled.ThData>학과.전공</Styled.ThData>
+            <Styled.ThData>동아리</Styled.ThData>
+            <Styled.ThData>직책</Styled.ThData>
+            <Styled.ThData>가입일자</Styled.ThData>
+            <Styled.ThData>소속대학</Styled.ThData>
+            <Styled.ThBorder>체크</Styled.ThBorder>
+          </tr>
+        </thead>
+        <GroupMembers members={groupMembers} />
+      </Styled.Table>
+    </div>
+  );
+};
 
 export default GroupMemberList;
