669e148f8295054bab2e2b68427a7ef887ebe969
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-12 15:52:44 +0900

    학번 post api 변경

--- client/src/api/SendToServer.ts
+++ client/src/api/SendToServer.ts
@@ -3,7 +3,7 @@ import {studentQr} from "../types/QrType/StudentQr";
 
 export const SendToServer = (data: studentQr) => {
     axios
-        .post("https://19e6afb7-2763-439d-bebf-183abf40a711.mock.pstmn.io/api/check-in",data)
+        .post("http://34.47.97.81:8080/api/events/check-in",data)
         .then((res) => {
             console.log(res);
         })
