28df348ef53c31dce551c1ae81d660b4772bc0ac
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-12 15:58:01 +0900

    MainQrCheckIn 페이지 수정
    padding 값 130px늘렸습니다

--- client/src/pages/QrCheckIn/MainQrCheckIn.styles.ts
+++ client/src/pages/QrCheckIn/MainQrCheckIn.styles.ts
@@ -1,35 +1,34 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-around;
     width: 100%;
     height: 100%;
-
     padding:0;
     margin:0;
     background-color: #F0F4F7;
 `;
 
-export const Event = styled.div`
+const Event = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     background-color: white;
     border-radius: 20px;
-    padding: 20px;
+    padding: 150px;
 `
 
-export const Date = styled.div`
+const GroupBox = styled.div`
     width: 100%;
     height: 30%;
-    border: 1px solid black;
+    border-top: 1px solid lightgray;
 `
 
-export const QrStyle = styled.div`
-    border-radius: 25px;
-    background-color: #a18ee8;
-    border: 15px solid #a18ee8;
-    padding:10px;
+const EventTime = styled.div`
+    display: flex;
+    flex-direction: row;
 `
+
+export {Wrapper, Event, GroupBox,  EventTime};
--- client/src/pages/QrCheckIn/MainQrCheckIn.tsx
+++ client/src/pages/QrCheckIn/MainQrCheckIn.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import QrScan from './QrScan'
 import * as Styled from './MainQrCheckIn.styles';
+import QrScan from '../../components/QrCheckIn/QrScan';
 
 const MainQrCheckIn = () => {
     return (
        <Styled.Wrapper>
-           <Styled.Event>
-               <h2>EVENT NAME</h2>
-               <div>
-                   <h3>2024.08.05</h3>
-                   <h3>2024.08.05</h3>
-                </div>
-                <Styled.Date>
-                    SELECT GROUP
-                </Styled.Date>
-           </Styled.Event>
+         <Styled.Event>
+           <h2>EVENT NAME</h2>
+           <Styled.EventTime>
+             <h3>2024.09.13 18:30</h3>
+             <h3>~</h3>
+             <h3>2024.09.13 20:00</h3>
+           </Styled.EventTime>
+           <Styled.GroupBox>
+             참가 가능한 그룹
+           </Styled.GroupBox>
+         </Styled.Event>
            <div className="Qr">
-               <Styled.QrStyle><QrScan/></Styled.QrStyle>
+               <QrScan/>
                <h2>QR CODE를 화면의 사각형 안에 맞춰주세요.</h2>
            </div>
        </Styled.Wrapper>
