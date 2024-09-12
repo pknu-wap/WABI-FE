038832d8234d89d7efd75f9cc0a1f8fc8e083d80
wkdghdwns199 <wkdghdwns199@naver.com>
2024-09-13 03:52:24 +0900

    #13 feature : 페이지 Routing 기능 추가
    BrowserRouter, Routes, Route 컴포넌트들로 페이지 routing 기능 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
-import GroupDetail from './pages/GroupDetail/GroupDetail';
+import MainQrCheckIn from './pages/QrCheckIn/MainQrCheckIn';
+import EventDetail from './pages/EventDetail/EventDetail';
 
 function App() {
-
-
   return (
-    <div className="App">
-      <GroupDetail />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/qrScan" element={<MainQrCheckIn />} />
+        <Route path="/" element={<EventDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
-
 }
 export default App;
-
