5fc2a0076e5d7f51ec2454c8a0ccf453d077c291
wkdghdwns199 <wkdghdwns199@naver.com>
2024-09-13 03:55:44 +0900

    #13 feature : event_detail page 스타일 생성
    이벤트에 참가 예정인 인원 보여주는 event_detail 페이지 생성

---
+++ client/src/pages/EventDetail/EventDetail.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  background-color: #f0f4f7;
+`;
+
+export {Wrapper};
