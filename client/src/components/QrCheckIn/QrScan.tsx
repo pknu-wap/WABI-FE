27149dadae75b5afa544e23e41ba5722096600f0
하윤정 <112613300+YJeongs@users.noreply.github.com>
2024-09-13 00:22:45 +0900

    Main_qr_check_in 페이지 UI 구현

--- client/src/components/QrCheckIn/QrScan.tsx
+++ client/src/components/QrCheckIn/QrScan.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import QrScanner from 'react-qr-scanner';
-import {SendToServer} from '../../api/sendToServer';
+import {sendToServer} from '../../api/SendToServer';
 import {studentQr} from '../../types/QrType/StudentQr';
 import {scanData} from '../../types/QrType/ScanData';
 import * as Styled from '../QrCheckIn/qrCode.styles';
@@ -14,7 +14,11 @@ const handleError = (err: Error) => {
   console.error(err);
 };
 
-const QrScan = () => {
+interface QrScanProps {
+  onScanResult: (message: string, messageColor:string, qrColor:string) => void; // Callback prop for scan result message
+}
+
+const QrScan = ({ onScanResult }: QrScanProps) => {
   const [scanned, setQrScanned] = useState(false);
   const [nextScanned, setNextScanned] = useState(0);
 
@@ -28,18 +32,28 @@ const QrScan = () => {
         studentIdEndPoint,
       );
       Student.studentId = ExtractedStudentId;
-      SendToServer(Student);
 
-      setQrScanned(true);
+        sendToServer(Student)
+          .then((res) => {
+            if (!res.data.message || res.data.message == "이미 체크인 했습니다.") {
+              onScanResult("정상적으로 참석되었습니다.",'#4E54F5','#4E54F5');
+            } else {
+              onScanResult("이벤트 해당그룹이 아닙니다.",'red','red');
+            }
+            setQrScanned(true);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
       setTimeout(() => {
         setQrScanned(false);
+        onScanResult("QR CODE를 화면의 사각형 안에 맞춰주세요.",'black','lightgray');
         setNextScanned(prevKey => prevKey + 1);
       }, 1000);
 
     }
   };
-
 
   return (
     <div>
