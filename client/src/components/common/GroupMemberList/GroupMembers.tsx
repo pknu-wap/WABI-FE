702ecc80983422e6f5c648aa0019ea81a3c25e32
wkdghdwns199 <wkdghdwns199@naver.com>
2024-09-12 12:58:30 +0900

    #13 refactor : map 함수에 key 값 적용
    map 함수에 key 값 적용 후 랜더링

--- client/src/components/common/GroupMemberList/GroupMembers.tsx
+++ client/src/components/common/GroupMemberList/GroupMembers.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import {students, student} from "../../../types/studentTypes";
-import GroupMember from "./GroupMember";
+import React from 'react';
+import {students, student} from '../../../types/studentTypes';
+import GroupMember from './GroupMember';
 
-
-const GroupMembers = ({ members }: { members: students }) => {
-    return (
-        <>
-        {members.map((member: student) => (
-                <GroupMember member={member} />
-            ))}
-        </>
-    )
-}
+const GroupMembers = ({members}: {members: students}) => {
+  return (
+    <>
+      {members.map((member: student, index: number) => (
+        <GroupMember key={index} member={member} />
+      ))}
+    </>
+  );
+};
 
 export default GroupMembers;
