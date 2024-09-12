b2da4872a9aff65197243133094985224be89692
wkdghdwns199 <wkdghdwns199@naver.com>
2024-09-13 03:58:46 +0900

    #13 fix : QrCheckIn 페이지 헤더 추가를 위한 상위 태그 변경

--- client/src/pages/QrCheckIn/MainQrCheckIn.tsx
+++ client/src/pages/QrCheckIn/MainQrCheckIn.tsx
@@ -1,27 +1,29 @@
-import React from "react";
+import React from 'react';
 import * as Styled from './MainQrCheckIn.styles';
 import QrScan from '../../components/QrCheckIn/QrScan';
+import Header from '../../components/common/Header/Header';
 
 const MainQrCheckIn = () => {
-    return (
-       <Styled.Wrapper>
-         <Styled.Event>
-           <h2>EVENT NAME</h2>
-           <Styled.EventTime>
-             <h3>2024.09.13 18:30</h3>
-             <h3>~</h3>
-             <h3>2024.09.13 20:00</h3>
-           </Styled.EventTime>
-           <Styled.GroupBox>
-             참가 가능한 그룹
-           </Styled.GroupBox>
-         </Styled.Event>
-           <div className="Qr">
-               <QrScan/>
-               <h2>QR CODE를 화면의 사각형 안에 맞춰주세요.</h2>
-           </div>
-       </Styled.Wrapper>
-    )
-}
+  return (
+    <>
+      <Header />
+      <Styled.Wrapper>
+        <Styled.Event>
+          <h2>EVENT NAME</h2>
+          <Styled.EventTime>
+            <h3>2024.09.13 18:30</h3>
+            <h3>~</h3>
+            <h3>2024.09.13 20:00</h3>
+          </Styled.EventTime>
+          <Styled.GroupBox>참가 가능한 그룹</Styled.GroupBox>
+        </Styled.Event>
+        <div className="Qr">
+          <QrScan />
+          <h2>QR CODE를 화면의 사각형 안에 맞춰주세요.</h2>
+        </div>
+      </Styled.Wrapper>
+    </>
+  );
+};
 
 export default MainQrCheckIn;
