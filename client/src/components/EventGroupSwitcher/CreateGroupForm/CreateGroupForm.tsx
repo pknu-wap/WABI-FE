01f4de622c37e778e63c2419cfbe471005f99b5d
Junseo Kim <semiwest2@gmail.com>
2024-09-16 17:33:29 +0900

    #33 feature : 그룹 개설폼 CreateGroupForm.tsx 컴포넌트 제작
    차후 api 적용으로 추가기능 구현 예정

---
+++ client/src/components/EventGroupSwitcher/CreateGroupForm/CreateGroupForm.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CommonFormLayout from 'components/common/CommonFormLayout/CommonFormLayout';
+import InputField from '../../common/InputField/InputField';
+const CreateGroupForm = () => {
+  //Todo
+  //차후 수정하기기능을 위해 데이터를 받아와서 띄우도록 추가 예정
+
+  const handleSave = () => {
+    //Todo
+    //그룹 개설폼에서 저장하기 눌렀을때의 api 동작로직 추가
+  };
+
+  const handleDelete = () => {
+    //Todo
+    //그룹 개설폼에서 삭제하기 눌렀을때의 api 동작로직 추가
+  };
+
+  return (
+    <CommonFormLayout
+      title="그룹 정보"
+      onSubmit={handleSave}
+      onDelete={handleDelete}
+    >
+      <InputField label={'그룹명'} placeholder={'그룹명'} required={true} />
+    </CommonFormLayout>
+  );
+};
+
+export default CreateGroupForm;
