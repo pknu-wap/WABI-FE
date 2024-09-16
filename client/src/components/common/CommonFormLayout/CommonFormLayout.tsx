30b4080929ddab24a0db8c25c64cb3c70b23f552
Junseo Kim <semiwest2@gmail.com>
2024-09-16 17:12:16 +0900

    #33 [feature]Event, Group 개설 입력을 위한 CommonFormLayout 컴포넌트 제작
    title, children, onSubmit, onDelete을 props로 받는다.
    
    CreateEventForm와 GroupEventForm 컴포넌트의 틀이 됩니다.

---
+++ client/src/components/common/CommonFormLayout/CommonFormLayout.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import * as Styled from 'components/common/CommonFormLayout/CommonFormLayout.styles';
+import Button from '../Button/Button';
+
+interface CreateFormLayoutProps {
+  title: string;
+  children: React.ReactNode;
+  onSubmit: () => void;
+  onDelete?: () => void;
+}
+
+const CommonFormLayout: React.FC<CreateFormLayoutProps> = ({
+  title,
+  children,
+  onSubmit,
+  onDelete,
+}) => {
+  return (
+    <Styled.FormContainer>
+      <Styled.FormTitle>{title}</Styled.FormTitle>
+      {onDelete && (
+        <Styled.DeleteButton onClick={onDelete}>
+          <Styled.DeleteIcon
+            src="images/Icon/DeleteButtonIcon.png"
+            alt="Delete"
+          />
+        </Styled.DeleteButton>
+      )}
+      <Styled.Form onSubmit={onSubmit}>
+        {children}
+        <Button
+          type="submit"
+          width="106px"
+          height="40px"
+          buttonColor="#4E54F5"
+          borderColor="#C1C7CD"
+          borderRadius="10px"
+          fontColor="#FFFFFF"
+          fontSize="16px"
+        >
+          저장하기
+        </Button>
+      </Styled.Form>
+    </Styled.FormContainer>
+  );
+};
+
+export default CommonFormLayout;
