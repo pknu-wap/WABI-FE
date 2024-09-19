bd3fe12f26bf40b0fc7a1b15f643f134684b2060
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-19 23:27:22 +0900

    이벤트리스트 스타일 수정
    테이블에서 grid로 변경

--- client/src/components/EventGroupSwitcher/EventCardList/EventCardList.styles.ts
+++ client/src/components/EventGroupSwitcher/EventCardList/EventCardList.styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
-export const EventList = styled.table`
-    border-collapse: separate;
-    border-spacing: 30px 30px;
+export const EventListGrid = styled.div`
+  display: grid;
+  grid-template-columns: 430px 430px;
+  gap: 15px;
+  background-color: #ccc;
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
