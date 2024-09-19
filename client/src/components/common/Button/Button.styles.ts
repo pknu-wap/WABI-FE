653f4c4a4f7f99a14a09134a884ac79418f9f67e
Junseo Kim <semiwest2@gmail.com>
2024-09-20 01:51:42 +0900

    #38 Fix : 공용 Button 컴포넌트 props 수정
    hasBorder가 string인데 ''가 빠져있어서 수정

--- client/src/components/common/Button/Button.styles.ts
+++ client/src/components/common/Button/Button.styles.ts
@@ -1,40 +1,40 @@
-import styled,{css} from "styled-components";
+import styled, {css} from 'styled-components';
 
 //TODO
 // hover 처리도 추가하기
 
 export interface ButtonStyle {
-    width?: string;
-    height?: string;
-    buttonColor?: string;
-    hasBorder?: string;
-    borderColor?: string;
-    borderRadius?: string;
-    fontColor?: string;
-    fontSize?: string;
+  width?: string;
+  height?: string;
+  buttonColor?: string;
+  hasBorder?: string;
+  borderColor?: string;
+  borderRadius?: string;
+  fontColor?: string;
+  fontSize?: string;
 }
 
 export const ButtonStyled = styled.button<ButtonStyle>`
-    cursor: pointer;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    ${({
-        width = 'auto',
-        height = 'auto',
-        buttonColor = 'auto',
-        hasBorder = false,
-        borderColor = 'white',
-        borderRadius = 'auto',
-        fontColor = 'auto',
-        fontSize = 'auto',
-    }) => css`
-        width: ${width};
-        height: ${height};
-        background-color: ${buttonColor};
-        border: ${hasBorder ? `1px solid ${borderColor}` : 'none'};
-        border-radius: ${borderRadius};
-        color: ${fontColor};
-        font-size: ${fontSize};
-    `}
-`
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  ${({
+    width = 'auto',
+    height = 'auto',
+    buttonColor = 'auto',
+    hasBorder = 'false',
+    borderColor = 'white',
+    borderRadius = 'auto',
+    fontColor = 'auto',
+    fontSize = 'auto',
+  }) => css`
+    width: ${width};
+    height: ${height};
+    background-color: ${buttonColor};
+    border: ${hasBorder ? `1px solid ${borderColor}` : 'none'};
+    border-radius: ${borderRadius};
+    color: ${fontColor};
+    font-size: ${fontSize};
+  `}
+`;
