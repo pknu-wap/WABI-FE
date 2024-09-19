52fa3305f9da7902aac6ff989edbdbeff2d762bc
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-19 23:26:56 +0900

    이벤트리스트 컴포넌트 수정
    1. 임포트 절대경로로 수정
    
    2. 테이블이 아닌 grid로 변경

--- client/src/components/EventGroupSwitcher/EventCardList/EventCardList.tsx
+++ client/src/components/EventGroupSwitcher/EventCardList/EventCardList.tsx
@@ -1,22 +1,13 @@
-import React from "react";
-import EventCard from "../EventCard/EventCard";
-import * as Styled from "./EventCardList.styles";
+import React from 'react';
+import EventCard from 'components/EventGroupSwitcher/EventCard/EventCard';
+import * as Styled from 'components/EventGroupSwitcher/EventCardList/EventCardList.styles';
 
 const EventCardList = () => {
-    return (
-            <Styled.EventList>
-                <tbody>
-                <tr>
-                    <td><EventCard /></td>
-                    <td><EventCard /></td>
-                </tr>
-                <tr>
-                    <td><EventCard /></td>
-                    <td><EventCard /></td>
-                </tr>
-                </tbody>
-            </Styled.EventList>
-    );
-}
+  return (
+    <Styled.EventListGrid>
+      <EventCard />
+    </Styled.EventListGrid>
+  );
+};
 
 export default EventCardList;
