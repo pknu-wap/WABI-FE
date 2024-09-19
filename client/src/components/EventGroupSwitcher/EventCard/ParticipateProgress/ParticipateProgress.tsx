fb3688d1c49624bd17d1d532da4a527ffedffed6
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-19 23:28:59 +0900

    prettier 적용

--- client/src/components/EventGroupSwitcher/EventCard/ParticipateProgress/ParticipateProgress.tsx
+++ client/src/components/EventGroupSwitcher/EventCard/ParticipateProgress/ParticipateProgress.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import * as Styled from "./ParticipateProgress.styles";
+import React from 'react';
+import * as Styled from './ParticipateProgress.styles';
 
 const ParticipateProgress = () => {
-    return (
-        <Styled.ParticipateProgressBox>
-            <Styled.ParticipateNum>
-                <h3>참여 수</h3>
-                <h3>10 / 50 명</h3>
-            </Styled.ParticipateNum>
-            <Styled.Progress/>
-        </Styled.ParticipateProgressBox>
-    )
-}
+  return (
+    <Styled.ParticipateProgressBox>
+      <Styled.ParticipateNum>
+        <h3>참여 수</h3>
+        <h3>10 / 50 명</h3>
+      </Styled.ParticipateNum>
+      <Styled.Progress />
+    </Styled.ParticipateProgressBox>
+  );
+};
 
 export default ParticipateProgress;
