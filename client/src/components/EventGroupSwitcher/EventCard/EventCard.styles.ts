8c7ef6011d69f1319bfcff1f3a659e9d9743a760
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-19 23:32:53 +0900

    prettier적용

--- client/src/components/EventGroupSwitcher/EventCard/EventCard.styles.ts
+++ client/src/components/EventGroupSwitcher/EventCard/EventCard.styles.ts
@@ -1,51 +1,51 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Card = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 400px;
-    height: 200px;
-    border: 3px solid white;
-    border-radius: 15px;
-    padding: 0px 10px 10px 15px;
-    cursor: pointer;
-    &:hover {
-        border: 3px solid #4E54F5;
-    }
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  height: 200px;
+  border: 3px solid white;
+  border-radius: 15px;
+  padding: 0px 10px 10px 15px;
+
+  &:hover {
+    border: 3px solid #4e54f5;
+  }
+  background-color: white;
 `;
 
-
 export const EventTitle = styled.div`
-    height: 40px;
-    display: flex;
-    justify-content: space-between;
-    img{
-        width: 25px;
-        height: 25px;
-        cursor: pointer;
-        margin-top: 20px;
-    }
-`
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  img {
+    width: 25px;
+    height: 25px;
+    cursor: pointer;
+    margin-top: 20px;
+  }
+`;
 
 export const EventTime = styled.div`
-    display: flex;
-    flex-direction: row;
-    h3 {
-        font-size: 15px;
-    }
-    span{
-        color: #4E54F5;
-    }
+  display: flex;
+  flex-direction: row;
+  h3 {
+    font-size: 15px;
+  }
+  span {
+    color: #4e54f5;
+  }
 `;
 
 export const Group = styled.div`
-    display: flex;
-    flex-direction: column;
-    h3 {
-        font-size: 15px;
-        color: lightgray;
-        margin: 0;
-        padding: 0; 
-        line-height: 1.2;
-    }
+  display: flex;
+  flex-direction: column;
+  h3 {
+    font-size: 15px;
+    color: lightgray;
+    margin: 0;
+    padding: 0;
+    line-height: 1.2;
+  }
 `;
