7e879cdda5ec4b8e92cbd49816afa7e58f382853
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-19 23:30:13 +0900

    참여자수 게이지 주석 추가

--- client/src/components/EventGroupSwitcher/EventCard/ParticipateProgress/ParticipateProgress.styles.ts
+++ client/src/components/EventGroupSwitcher/EventCard/ParticipateProgress/ParticipateProgress.styles.ts
@@ -1,25 +1,29 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ParticipateProgressBox = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const ParticipateNum = styled.div`
-    display: flex;
-    justify-content: space-between; 
-    align-items: center; 
-    width: 100%;
-    margin-top: 10px;
-    h3{
-        font-size: 15px;
-        line-height: 0.1;
-    }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 10px;
+  h3 {
+    font-size: 15px;
+    line-height: 0.1;
+  }
 `;
 
 export const Progress = styled.div`
-    width: 400px;
-    height: 10px;
-    background-color: #4E54F5;
-    border-radius: 5px;
+  width: 400px;
+  height: 10px;
+  background-color: #4e54f5;
+  border-radius: 5px;
+`;
+
+export const Gauge = styled.div`
+  // 여기에 props로 참여자 수 받아서 backgroud-color 조정하는 코드 추가 예정
 `;
