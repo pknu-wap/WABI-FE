f85273fa3b14b88fd0cdf23949144bbb5ca0ee7b
하윤정 <112613300+YJeongs@users.noreply.github.com>
2024-09-20 18:25:39 +0900

    feature: 학번 중복 스캔시 처리 로직 추가

--- client/src/components/QrCheckIn/QrScan.tsx
+++ client/src/components/QrCheckIn/QrScan.tsx
@@ -40,12 +40,15 @@ const QrScan = ({onScanResult}: QrScanProps) => {
       sendToServer(Student)
         .then(res => {
           if (
-            !res.data.message ||
-            res.data.message === '이미 체크인 했습니다.'
+            !res.data.message
           ) {
             onScanResult('정상적으로 참석되었습니다.', '#4E54F5', '#4E54F5');
+          } else if (
+              res.data.message === '이미 체크인 했습니다.'
+          ) {
+            onScanResult('이미 참석되었습니다.','#F5C400','#F5C400');
           } else {
-            onScanResult('이벤트 해당그룹이 아닙니다.', 'red', 'red');
+            onScanResult('이벤트 해당그룹이 아닙니다.', '#FF7078', '#FF7078');
           }
           setQrScanned(true);
         })
