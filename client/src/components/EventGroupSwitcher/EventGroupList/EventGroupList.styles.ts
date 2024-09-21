e631f718e994df149c46fff61e3f26ab5aa98a42
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-21 20:14:21 +0900

    그룹카드리스트 크기 줄임 및 margin설정

--- client/src/components/EventGroupSwitcher/EventGroupList/EventGroupList.styles.ts
+++ client/src/components/EventGroupSwitcher/EventGroupList/EventGroupList.styles.ts
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const GroupListGrid = styled.div`
   display: grid;
-  grid-template-columns: 289px 289px 289px;
-  gap: 15px;
+  grid-template-columns: 249px 249px 249px;
+  height: 100%;
+  gap: 24px;
+  margin: 20px 16px;
   background-color: #ccc;
   @media (max-width: 768px) {
     grid-template-columns: 2fr;
