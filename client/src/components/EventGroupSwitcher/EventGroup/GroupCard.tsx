50190a9b4e0e9e54ed7dafc452da761148685428
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-21 20:14:01 +0900

    그룹카드 클릭시 bandId 저장로직 추가

--- client/src/components/EventGroupSwitcher/EventGroup/GroupCard.tsx
+++ client/src/components/EventGroupSwitcher/EventGroup/GroupCard.tsx
@@ -2,9 +2,13 @@ import React, {useEffect, useState} from 'react';
 import {LoadGroupList} from 'api/loadGroupList';
 import {Band} from 'types/eventTypes';
 import * as Styled from 'components/EventGroupSwitcher/EventGroup/GroupCard.styles';
+import {useRecoilState} from 'recoil';
+import {selectedBandIdState} from 'recoil/currentBandId';
 
 const GroupCard = () => {
   const [groups, setGroupList] = useState<Band[]>([]); // State to store event data
+  const [selectedBandId, setSelectedBandId] =
+    useRecoilState(selectedBandIdState);
 
   useEffect(() => {
     LoadGroupList().then(filteredEvent => {
@@ -12,10 +16,14 @@ const GroupCard = () => {
     });
   }, []);
 
+  const handleCardClick = (bandId: number) => {
+    setSelectedBandId(bandId);
+  };
+
   return (
     <>
       {groups.map((group: Band) => (
-        <Styled.GroupCard>
+        <Styled.GroupCard onClick={() => handleCardClick(group.bandId)}>
           <Styled.GroupTitle>
             <h2>{group.bandName}</h2>
           </Styled.GroupTitle>
