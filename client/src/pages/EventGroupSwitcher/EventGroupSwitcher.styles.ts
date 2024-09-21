6dff634be371c8f4bac603915f15759dbcccff3a
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-21 20:15:08 +0900

    이벤트리스트와 수정폼을 담는 스타일

---
+++ client/src/pages/EventGroupSwitcher/EventGroupSwitcher.styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const EventGroupWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+export const DivideLine = styled.hr`
+    margin : 0;
+    border: 1px solid #C1C7CD;
+`
