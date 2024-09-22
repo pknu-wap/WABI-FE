24c3a47039af1e1ac842d9955b7360cca856a891
Junseo Kim <semiwest2@gmail.com>
2024-09-22 21:47:54 +0900

    #69 fix : 페이지명 변경 이후 Route 경로 이름 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import MainQrCheckIn from './pages/QrCheckIn/MainQrCheckIn';
 import EventDetail from './pages/EventDetail/EventDetail';
-import EventGroupSwitcher from './pages/EventGroupSwitcher/EventGroupSwitcher';
+import EventAndGroupList from './pages/EventAndGroupList/EventAndGroupList';
 import GrouupDetail from 'pages/GroupDetail/GroupDetail';
 import MainEventList from './pages/MainEventList/MainEventList';
 
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<EventGroupSwitcher />} />
+        <Route path="/" element={<EventAndGroupList />} />
         <Route path="/eventDetail" element={<EventDetail />} />
         <Route path="/qrScan" element={<MainQrCheckIn />} />
         <Route path="/groupDetail" element={<GrouupDetail />} />
