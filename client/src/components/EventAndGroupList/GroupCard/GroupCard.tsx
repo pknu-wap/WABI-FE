6a2ef04e5bc5a2d8443559ca1e63079457658076
Junseo Kim <semiwest2@gmail.com>
2024-09-23 14:02:27 +0900

    #73 Fix : GroupCard 구조 수정 및 상태 변경 함수 props로 전달
    1. GroupCard에 들어있던 map을 GroupCardList로 옮김
    2. props로 setSelectedBandId가 들어있는 handleCardClick를 받는다.

--- client/src/components/EventAndGroupList/GroupCard/GroupCard.tsx
+++ client/src/components/EventAndGroupList/GroupCard/GroupCard.tsx
@@ -1,43 +1,39 @@
-import React, {useEffect, useState} from 'react';
-import {LoadGroupList} from 'api/loadGroupList';
+import React from 'react';
 import {Band} from 'types/groupTypes';
 import * as Styled from 'components/EventAndGroupList/GroupCard/GroupCard.styles';
-import {useRecoilState} from 'recoil';
-import {selectedBandIdState} from 'recoil/currentBandId';
+import {useNavigate} from 'react-router-dom';
 
-const GroupCard = () => {
-  const [groups, setGroupList] = useState<Band[]>([]); // State to store event data
-  const [selectedBandId, setSelectedBandId] =
-    useRecoilState(selectedBandIdState);
+interface GroupCardProps {
+  group: Band;
+  onUpdateClick: (bandId: number) => void;
+  isSelected: boolean;
+}
 
-  useEffect(() => {
-    LoadGroupList().then(filteredEvent => {
-      setGroupList(filteredEvent);
-    });
-  }, []);
+const GroupCard = ({group, onUpdateClick, isSelected}: GroupCardProps) => {
+  const navigate = useNavigate();
 
   const handleCardClick = (bandId: number) => {
-    setSelectedBandId(bandId);
+    navigate('/groupDetail', {state: {bandId}});
   };
 
   return (
-    <>
-      {groups.map((group: Band) => (
-        <Styled.GroupCard
-          className={selectedBandId === group.bandId ? 'selected' : ''}
+    <Styled.GroupCard
+      onClick={() => handleCardClick(group.bandId)}
+      className={isSelected ? 'selected' : ''}
+    >
+      <Styled.GroupTitle>
+        <h2>{group.bandName}</h2>
+        <Styled.UpdateButton
+          onClick={e => {
+            e.stopPropagation();
+            onUpdateClick(group.bandId);
+          }}
         >
-          <Styled.GroupTitle>
-            <h2>{group.bandName}</h2>
-            <img
-              onClick={() => handleCardClick(group.bandId)}
-              src="/images/EditEvent.png"
-              alt="EditEvenIcon"
-            />
-          </Styled.GroupTitle>
-          <h3>50명</h3>
-        </Styled.GroupCard>
-      ))}
-    </>
+          <Styled.UpdateIcon src="/images/EditEvent.png" alt="EditEvenIcon" />
+        </Styled.UpdateButton>
+      </Styled.GroupTitle>
+      <h3>50명</h3>
+    </Styled.GroupCard>
   );
 };
 
