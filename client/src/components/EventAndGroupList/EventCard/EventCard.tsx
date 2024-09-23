dd0359663e978c67f2f7d023d3d13a45c8bbde3e
Junseo Kim <semiwest2@gmail.com>
2024-09-23 13:56:50 +0900

    #73 Fix : EventCard 구조 수정 및 상태 변경 함수 props로 전달
    1. EventCard에 들어있던 map을 EventCardList로 옮김
    2. props로 setSelectedEventId가 들어있는 handleCardClick를 받는다.

--- client/src/components/EventAndGroupList/EventCard/EventCard.tsx
+++ client/src/components/EventAndGroupList/EventCard/EventCard.tsx
@@ -1,76 +1,73 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import * as Styled from 'components/EventAndGroupList/EventCard/EventCard.styles';
 import {CheckInInfo} from 'types/eventTypes';
 import {Band} from 'types/groupTypes';
-import {LoadEventList} from 'api/loadEventList';
-import {useRecoilState} from 'recoil';
-import {selectedEventIdState} from 'recoil/currentEventId';
 import ParticipateProgress from 'components/EventAndGroupList/ParticipateProgress/ParticipateProgress';
+import {useNavigate} from 'react-router-dom';
 
-const EventCard = () => {
-  const [events, setEvents] = useState<CheckInInfo[]>([]);
-  const [selectedEventId, setSelectedEventId] =
-    useRecoilState(selectedEventIdState);
+interface EventCardProps {
+  event: CheckInInfo;
+  onUpdateClick: (eventId: number) => void;
+  selectedEventId: number | null;
+}
 
-  useEffect(() => {
-    LoadEventList().then(filteredEvent => {
-      setEvents(filteredEvent);
-    });
-  }, []);
+const EventCard = ({event, onUpdateClick, selectedEventId}: EventCardProps) => {
+  const navigate = useNavigate();
 
   const handleCardClick = (eventId: number) => {
-    setSelectedEventId(eventId);
+    if (eventId) {
+      navigate('/eventDetail', {state: {eventId}});
+    }
   };
 
   return (
-    <>
-      {events.map((event: CheckInInfo) => (
-        <Styled.Card
-          key={event.eventId}
-          className={selectedEventId === event.eventId ? 'selected' : ''}
+    <Styled.Card
+      className={selectedEventId === event.eventId ? 'selected' : ''}
+      onClick={() => handleCardClick(event.eventId)}
+    >
+      <Styled.EventTitle>
+        <h2>{event.eventName}</h2>
+        <Styled.UpdateButton
+          onClick={e => {
+            e.stopPropagation();
+            onUpdateClick(event.eventId);
+          }}
         >
-          <Styled.EventTitle>
-            <h2>{event.eventName}</h2>
-            <img
-              onClick={() => handleCardClick(event.eventId)}
-              src="/images/EditEvent.png"
-              alt="EditEvenIcon"
-            />
-          </Styled.EventTitle>
-          <Styled.EventTime>
-            <h3>
-              {event.startAt} &nbsp;&nbsp;<span>{'>'}</span>&nbsp;&nbsp;
-            </h3>
-            <h3>{event.endAt}</h3>
-          </Styled.EventTime>
-          <Styled.Group>
-            {event.bands.length >= 3 ? (
-              <>
-                {event.bands.slice(0, 3).map((band: Band) => (
-                  <h3 key={band.bandId}>{band.bandName}</h3>
-                ))}
-                <h3>외 {event.bands.length - 3}개</h3>
-              </>
-            ) : (
-              <>
-                {event.bands.map((band: Band) => (
-                  <h3 key={band.bandId}>{band.bandName}</h3>
-                ))}
-                {Array(3 - event.bands.length)
-                  .fill(null)
-                  .map((_, idx: number) => (
-                    <h3 key={`empty-${idx}`}>&nbsp;</h3>
-                  ))}
-              </>
-            )}
-          </Styled.Group>
-          <ParticipateProgress
-            checkIn={event.checkIn}
-            notCheckIn={event.notCheckIn}
-          />
-        </Styled.Card>
-      ))}
-    </>
+          <Styled.UpdateIcon src="/images/EditEvent.png" alt="EditEventIcon" />
+        </Styled.UpdateButton>
+      </Styled.EventTitle>
+      <Styled.EventTime>
+        <h3>
+          {event.startAt} &nbsp;&nbsp;<span>{'>'}</span>&nbsp;&nbsp;
+        </h3>
+        <h3>{event.endAt}</h3>
+      </Styled.EventTime>
+      <Styled.Group>
+        {event.bands.length >= 3 ? (
+          <>
+            {event.bands.slice(0, 3).map((band: Band) => (
+              <h3 key={band.bandId}>{band.bandName}</h3>
+            ))}
+            <h3>외 {event.bands.length - 3}개</h3>
+          </>
+        ) : (
+          <>
+            {event.bands.map((band: Band) => (
+              <h3 key={band.bandId}>{band.bandName}</h3>
+            ))}
+            {Array(3 - event.bands.length)
+              .fill(null)
+              .map((_, idx: number) => (
+                <h3 key={`empty-${idx}`}>&nbsp;</h3>
+              ))}
+          </>
+        )}
+      </Styled.Group>
+      <ParticipateProgress
+        checkIn={event.checkIn}
+        notCheckIn={event.notCheckIn}
+      />
+    </Styled.Card>
   );
 };
 
