c9df82a4f8abe6e4d084281201647981f29955b8
하윤정 <112613300+YJeongs@users.noreply.github.com>
2024-09-24 02:22:57 +0900

    fix: GroupMemberList UI 수정

--- client/src/components/common/GroupMemberList/GroupMemberList.styles.ts
+++ client/src/components/common/GroupMemberList/GroupMemberList.styles.ts
@@ -2,30 +2,33 @@ import styled from 'styled-components';
 export const Table = styled.table`
   border-collapse: separate;
   border-spacing: 0;
+  width: 70vw;
   background-color: #ffffff;
   border-radius: 10px;
+  color: #697077;
+  text-align: center;
+  margin-bottom: 10px;
 `;
 
 export const TBody = styled.tbody`
   tr:last-child td {
-    border-bottom: none;
+    //border-bottom: none;
   }
 `;
 
 export const ThData = styled.th`
-  padding: 10px;
-  border-bottom: 1px solid;
+  padding: 15px;
 `;
 
 export const ThBorder = styled(ThData)`
-  border-left: 1px solid #aeb5bc;
+  border-left: 1px solid #c1c7cd;
 `;
 
 export const TdData = styled.td`
-  padding: 10px;
-  border-bottom: 1px solid #aeb5bc;
+  padding: 15px;
+  border-top: 1px solid #c1c7cd;
 `;
 
 export const TdBorder = styled(TdData)`
-  border-left: 1px solid #aeb5bc;
+  border-left: 1px solid #c1c7cd;
 `;
