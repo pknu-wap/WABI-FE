8251797b22304eaa938b89e84b3a9814841606d6
하윤정 <112613300+YJeongs@users.noreply.github.com>
2024-09-24 00:31:48 +0900

    fix: groupId 동적 처리

--- client/src/api/manualUpload.ts
+++ client/src/api/manualUpload.ts
@@ -18,7 +18,7 @@ export const manualUpload = async (groupId: number, students: students) => {
     };
     console.log('requestBody:', requestBody);
     const response = await axios.post(
-      `https://zepelown.site/api/bands/6/members/enrollments/manual`,
+      `https://zepelown.site/api/bands/${groupId}/members/enrollments/manual`,
       requestBody,
     );
 
