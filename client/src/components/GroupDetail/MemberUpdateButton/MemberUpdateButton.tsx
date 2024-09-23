de6dec0763002e08741f02aedb6aae6d89bd319b
하윤정 <112613300+YJeongs@users.noreply.github.com>
2024-09-24 02:59:13 +0900

    feature: 버튼 스타일에 hover 추가 MemberUpdateButton에 반영 및

--- client/src/components/GroupDetail/MemberUpdateButton/MemberUpdateButton.tsx
+++ client/src/components/GroupDetail/MemberUpdateButton/MemberUpdateButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from "../../common/Button/Button";
+import Button from '../../common/Button/Button';
 //함수타입으로 정의 : () =>
 
 interface MemberUpdateButtonProps {
@@ -7,21 +7,26 @@ interface MemberUpdateButtonProps {
   isFileUpload?: boolean;
 }
 
-const MemberUpdateButton = ({ onClick, isFileUpload} : MemberUpdateButtonProps) => {
+const MemberUpdateButton = ({
+  onClick,
+  isFileUpload,
+}: MemberUpdateButtonProps) => {
   const buttonText = isFileUpload ? '인원 한 번에 등록' : '인원 수동 추가';
 
   return (
-      <Button
-          onClick={onClick}
-          height="40px"
-          borderRadius="10px"
-          fontSize="20px"
-          buttonColor="white"
-          padding="10px 16px 10px 16px"
-          margin="0 0 0 15px">
-        {buttonText}
-      </Button>
-  )
+    <Button
+      onClick={onClick}
+      height="40px"
+      borderRadius="10px"
+      fontSize="20px"
+      buttonColor="white"
+      padding="10px 16px 10px 16px"
+      margin="0 0 0 15px"
+      hoverFontColor="#4e54f5"
+    >
+      {buttonText}
+    </Button>
+  );
 };
 
 export default MemberUpdateButton;
