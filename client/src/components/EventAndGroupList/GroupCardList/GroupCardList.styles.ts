903926c84057f4b99bd1236fa0c445ce6870bb28
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-09-24 00:59:12 +0900

    #81 fix: 그룹 카드 리스트 grid스타일 수정
    Co-Authored-By: 김준서 Junseo Kim <semiwest2@gmail.com>

--- client/src/components/EventAndGroupList/GroupCardList/GroupCardList.styles.ts
+++ client/src/components/EventAndGroupList/GroupCardList/GroupCardList.styles.ts
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 export const GroupListGrid = styled.div`
   display: grid;
-  grid-template-columns: 249px 249px 249px;
+  grid-template-columns: repeat(3, 218px);
   height: 100%;
-  gap: 24px;
-  margin: 20px 16px;
-  background-color: white;
+  gap: 12px;
+  margin-left: 20px;
+  margin-top: 20px;
+
   @media (max-width: 768px) {
     grid-template-columns: 2fr;
   }
