a916033026305181cabd809e64ddd21e0863170b
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-10-04 14:48:44 +0900

    403에러 처리 로직 추가

--- client/src/api/loadEventList.tsx
+++ client/src/api/loadEventList.tsx
@@ -1,29 +1,46 @@
 import axios from 'axios';
 import {CheckInInfo} from 'types/eventTypes';
+import {logIn} from './logIn';
 
-export const LoadEventList = (token: string) => {
-  return axios
-    .get(`https://zepelown.site/api/events/list?adminId=1`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
+// 함수의 반환 타입을 Promise<CheckInInfo[]>로 명시
+export const LoadEventList = async (token: string): Promise<CheckInInfo[]> => {
+  try {
+    const response = await axios.get(
+      `https://zepelown.site/api/events/list?adminId=1`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       },
-    })
-    .then(res => {
-      const events = res.data.data;
+    );
 
-      return events.map((event: CheckInInfo) => ({
-        eventId: event.eventId,
-        eventName: event.eventName,
-        eventStudentMaxCount: event.eventStudentMaxCount,
-        startAt: event.startAt.slice(0, 10) + ' ' + event.startAt.slice(11, 16),
-        endAt: event.endAt.slice(0, 10) + ' ' + event.endAt.slice(11, 16),
-        bands: event.bands,
-        checkIn: event.checkInStatusCount.checkIn,
-        notCheckIn: event.checkInStatusCount.notCheckIn,
-      }));
-    })
-    .catch(error => {
+    const events = response.data.data;
+
+    return events.map((event: CheckInInfo) => ({
+      eventId: event.eventId,
+      eventName: event.eventName,
+      eventStudentMaxCount: event.eventStudentMaxCount,
+      startAt: `${event.startAt.slice(0, 10)} ${event.startAt.slice(11, 16)}`,
+      endAt: `${event.endAt.slice(0, 10)} ${event.endAt.slice(11, 16)}`,
+      bands: event.bands,
+      checkIn: event.checkInStatusCount.checkIn,
+      notCheckIn: event.checkInStatusCount.notCheckIn,
+    }));
+  } catch (error: any) {
+    if (error.response.status === 403) {
+      console.log('403 에러 발생, 토큰이 만료되었습니다. 재로그인 시도 중...');
+
+      try {
+        const newToken = await logIn('seongwon3', 'shin091612@@');
+        return LoadEventList(newToken);
+      } catch (loginError) {
+        console.log('로그인에 실패했습니다', loginError);
+        // 재로그인 기능 추가예정
+        return [];
+      }
+    } else {
       console.log('이벤트 리스트 불러오기에 실패했습니다', error);
       return [];
-    });
+    }
+  }
 };
