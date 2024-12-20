cdc91c066e909fd1fb43a908065be4f6edef6e93
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-12-21 00:39:45 +0900

    feat: 회원가입 로직 추가
    Co-Authored-By: 김준서 Junseo Kim <semiwest2@gmail.com>

--- client/src/components/Authentication/SignUpForm/SignUpForm.tsx
+++ client/src/components/Authentication/SignUpForm/SignUpForm.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {signUp} from '../../../api/signUp';
 import InputField from 'components/common/InputField/InputField';
 import CommonFormLayout from 'components/common/CommonFormLayout/CommonFormLayout';
 import * as Styled from 'components/Authentication/SignUpForm/SignUpForm.styles';
@@ -12,13 +13,60 @@ const SignInForm = () => {
     email: '',
   });
 
-  const handleSubmit = () => {
+  const validateUsernameInput = () => {
+    const usernameRegex = /^[a-z0-9]{4,10}$/;
+    if (!usernameRegex.test(loginFormData.username)) {
+      alert(
+        '알파벳 소문자와 숫자로만 구성된 최소 4자 이상, 10자 이하여야 합니다.',
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const validatePasswordInput = () => {
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[*~!@#])[A-Za-z\d*~!@#]{8,15}$/;
+    if (!passwordRegex.test(loginFormData.password)) {
+      alert(
+        '비밀번호는 8자 이상, 15자 이하이며, 알파벳 대소문자, 숫자, 특수 문자(*~!@#)를 포함해야 합니다. 특수문자는 반드시 하나 이상 포함되어야 합니다.',
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const validateIsCorrectPasswordInput = () => {
     if (loginFormData.password !== loginFormData.passwordCheck) {
-      alert('비밀번호를 다시 확인해주세요');
+      alert('입력하신 비밀번호와 일치하지 않습니다.');
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = () => {
+    if (!validateUsernameInput()) {
+      return;
+    }
+    if (!validatePasswordInput()) {
+      return;
+    }
+    if (!validateIsCorrectPasswordInput()) {
       return;
     }
 
-    // 회원가입 API 호출 로직 추가
+    try {
+      const response = signUp(
+        loginFormData.username,
+        loginFormData.password,
+        loginFormData.email,
+      );
+      alert('회원가입에 성공하였습니다!');
+      return response;
+    } catch (error: any) {
+      console.error('회원가입 실패:', error);
+      alert('회원가입에 실패하였습니다. 다시 시도해주세요.');
+    }
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
